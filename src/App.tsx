895b597620d8aad1069de0b3c81ed8b0b101ca7f
antishanti1 <shutkova2603@gmail.com>
2024-04-02 16:53:42 -0400

    auth page set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import React from "react";
+import Auth from "./pages/auth";
+
 function App() {
   return (
-    <>
-      <h1>hello</h1>
-    </>
+    <div>
+      <Auth />
+    </div>
   );
 }
 
